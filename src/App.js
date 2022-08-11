7c85ec5b387450896a96995a7d63f49127209c59
jarocode <okolieuchenna890@gmail.com>
2022-08-11 19:25:06 +0100

    added routes, added jsconfig file to import components using  absolute paths

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import ChatRoom from "pages/chatroom";
+import Auth from "pages/auth";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Auth />} />
+          <Route path="/chatroom" exact element={<ChatRoom />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/pages/chatroom/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+import MainLayout from "layout/MainLayout";
+
+const index = () => {
+  return (
+    <MainLayout>
+      <Container>chat room</Container>
+    </MainLayout>
+  );
+};
+
+export default index;
+const Container = styled.div``;
